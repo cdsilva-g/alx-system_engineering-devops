d1589dcf64c91bf7a075587df5c4d708c957683b
cdsilva-g <christinegathuu@gmail.com>
2023-09-24 13:45:34 +0300

    missing closing brackets

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -4,8 +4,10 @@ file_line { "config file_name":
     ensure => 'present',
     line   => '    IdentityFile school'
     path   => 'etc/ssh/ssh_config'
+}
 
 file_line { "config password use":
     ensure => 'present'
     line   => '    PasswordAuthentication no'
     path   => 'etc/ssh/ssh_config'
+}
