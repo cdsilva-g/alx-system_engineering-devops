b8245779f9c96886da1b6c49c0beec0e7744e585
cdsilva-g <christinegathuu@gmail.com>
2023-09-24 13:48:05 +0300

    missing commas!

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,13 +1,13 @@
 # Configuring a config file using puppet
 
 file_line { "config file_name":
-    ensure => 'present',
-    line   => '    IdentityFile school'
-    path   => 'etc/ssh/ssh_config'
+    ensure => present,
+    line   => '    IdentityFile school',
+    path   => 'etc/ssh/ssh_config',
 }
 
 file_line { "config password use":
-    ensure => 'present'
-    line   => '    PasswordAuthentication no'
-    path   => 'etc/ssh/ssh_config'
+    ensure => present,
+    line   => '    PasswordAuthentication no',
+    path   => 'etc/ssh/ssh_config',
 }
