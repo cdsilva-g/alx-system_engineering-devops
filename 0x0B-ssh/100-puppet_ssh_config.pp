66b2f0ef9be67af93fac42f4aeea3abfc3abfb51
cdsilva-g <christinegathuu@gmail.com>
2023-09-24 13:33:25 +0300

    rearrangement

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,7 +1,7 @@
 # Configuring a config file using puppet
 
 exec { 'configure':
-    command => 'echo " iIdentityFile ~/.ssh/school\n  PasswordAuthentication no" >> etc/ssh/ssh_config',
     path    => 'usr/bin:/bin',
-    return  => [0,1]
+    command => 'echo "    IdentityFile ~/.ssh/school\n    PasswordAuthentication no" >> etc/ssh/ssh_config',
+    return  => [0,1],
 }
