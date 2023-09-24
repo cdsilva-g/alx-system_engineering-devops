a46294615ef91b09f232ea3c8e224769c58d4308
cdsilva-g <christinegathuu@gmail.com>
2023-09-24 13:02:38 +0300

    command path change

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,7 +1,7 @@
 # Configuring a config file using puppet
 
 exec { 'configure':
-    command => 'echo "IdentityFile ~/.ssh/school\n  PasswordAuthentication no" >> ~/.ssh/config',
+    command => 'echo " iIdentityFile ~/.ssh/school\n  PasswordAuthentication no" >> etc/ssh/ssh_config',
     path    => 'usr/bin:/bin',
     return  => [0,1]
 }
